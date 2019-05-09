89904b10aa4da632aa479bb3e568f902f9368b6e
SerenaFeng <serena.feng.711@gmail.com>
2019-05-09 15:44:34 +0800

    add swagger2html docker image

---
+++ Dockerfile
@@ -0,0 +1,17 @@
+FROM openjdk:8-jre-alpine
+MAINTAINER SerenaFeng
+
+WORKDIR /home
+
+RUN apk update && \
+    apk add ca-certificates wget bash asciidoctor && \
+    update-ca-certificates && \
+    wget -O swagger2markup.jar https://jcenter.bintray.com/io/github/swagger2markup/swagger2markup-cli/1.3.3/swagger2markup-cli-1.3.3.jar
+
+COPY ./convert.sh ./
+RUN chmod +x ./convert.sh
+
+ENV JAVA_OPTS="-Dfile.encoding=UTF-8 -Dsun.jnu.encoding=UTF-8"
+
+ENTRYPOINT ["./convert.sh"]
+
---
+++ convert.sh
@@ -0,0 +1,60 @@
+#!/bin/bash
+
+swagger2markup=/home/swagger2markup.jar
+fyamls=/opt
+linkcss=false
+out=/opt/_out
+
+while getopts ":i:t" arg; do
+  case ${arg} in
+    i) fyamls=/opt/${OPTARG};;
+    t) linkcss=true;;
+  esac
+done
+
+function convert {
+  local fyaml=${1}
+  local _opt=${fyaml#/opt/}
+  local obj=${out}/${_opt%.*}
+  local fadoc=${obj}.adoc
+  local fhtml=${obj}.html
+  local fcss=${obj##*/}.css
+
+  [[ ! -f ${fyaml} ]] && {
+    echo "${fyaml} not exist"
+    exit 1
+  }
+
+  echo "begin to convert to ${fadoc} ......"
+  adoccmd="convert -i ${fyaml} -f ${obj}"
+  [[ -f /opt/config.properties ]] && adoccmd="${adoccmd} -c config.properties"
+  java -jar ${swagger2markup} ${adoccmd}
+
+  echo "begin to convert to ${fhtml} ......"
+  htmlcmd="-b html5 -o ${obj}.html ${obj}.adoc"
+  [[ ${linkcss} == true ]] && htmlcmd="-a linkcss ${htmlcmd}"
+  asciidoctor ${htmlcmd}
+
+  [[ ${linkcss} == true ]] && {
+    mv "${obj%/*}/asciidoctor.css" ${obj}.css
+    sed -i "s/asciidoctor.css/${fcss}/g" ${fhtml}
+  }
+}
+
+function iterate_over_dir {
+  [[ -f ${1} ]] && {
+    [[ ${1} =~ yaml$|yml$ ]] && convert ${1}
+    return
+  }
+
+  for file in ${1%/}/*; do
+    [[ -d ${file} ]] && {
+      iterate_over_dir ${file} || true
+    } || {
+      [[ ${file} =~ yaml$|yml$ ]] && convert ${file}
+    }
+  done
+}
+
+iterate_over_dir ${fyamls}
+
